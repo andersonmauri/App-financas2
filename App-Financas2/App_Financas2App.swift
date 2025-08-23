1a6aa4b5878e1158100557ba8c1ab498027b8db3
Anderson Mauri <maurianderson@gmail.com>
2025-08-23 15:05:15 -0300

    Initial Commit

---
+++ App-Financas2/App_Financas2App.swift
@@ -0,0 +1,17 @@
+//
+//  App_Financas2App.swift
+//  App-Financas2
+//
+//  Created by Anderson Almeida on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct App_Financas2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-Financas2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App-Financas2
+//
+//  Created by Anderson Almeida on 23/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
